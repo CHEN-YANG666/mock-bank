bca68ec05216f8b40d8786cc99b6da949056c559
ChenYang <chenyang@example.com>
2026-02-01 11:33:32 +0900

    deploy web without firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'pages/shell_page.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  /// ✅ Firebase 初始化（Web / PWA 用）
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: "AIzaSyAabNJEJkWsQE5mVcYqc4ANo9vBmcH-VIc",
-      authDomain: "mock-bank-37875.firebaseapp.com",
-      projectId: "mock-bank-37875",
-      storageBucket: "mock-bank-37875.appspot.app",
-      messagingSenderId: "411423265063",
-      appId: "1:411423265063:web:c05cb769da193ac081d902",
-    ),
-  );
-
+void main() {
   runApp(const MyBankApp());
 }
 
@@ -27,16 +12,10 @@ class MyBankApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
-      /// ✅ 保留你原来的「银行风格」Theme
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
-        splashColor: Colors.transparent,
-        highlightColor: Colors.transparent,
-        hoverColor: Colors.transparent,
         useMaterial3: false,
       ),
-
       home: const ShellPage(),
     );
   }
